fb5810770a5f147c1ae61f49c1ca95adb1503fee
RaghdaHany <raghdahany550@gmail.com>
2019-05-14 12:37:01 +0200

    fix bugs in signin view model

--- app/src/main/java/com/travel/iti/travelapp/repository/networkmodule/ApiRequest.java
+++ app/src/main/java/com/travel/iti/travelapp/repository/networkmodule/ApiRequest.java
@@ -14,7 +14,7 @@ import retrofit2.http.POST;
 public interface  ApiRequest {
 
 
-    @POST("user/login/")
+    @POST("users/login")
     Call<ApiResponse<User>> SignIn(@Body User body);
 
 }
--- app/src/main/java/com/travel/iti/travelapp/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/travel/iti/travelapp/viewmodel/LoginViewModel.java
@@ -2,8 +2,10 @@ package com.travel.iti.travelapp.viewmodel;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.travel.iti.travelapp.repository.model.User;
 import com.travel.iti.travelapp.repository.networkmodule.ApiResponse;
@@ -23,6 +25,12 @@ public class LoginViewModel extends ViewModel {
     public MutableLiveData<String> Password = new MutableLiveData<>();
 
     private MutableLiveData<User> userMutableLiveData;
+
+    private Context mcontext;
+    public void init(Context context) {
+        this.mcontext=context;
+    }
+
     public MutableLiveData<User> getUser() {
 
         if (userMutableLiveData == null) {
@@ -39,20 +47,24 @@ public class LoginViewModel extends ViewModel {
         userMutableLiveData.setValue(loginUser);
 
     }
-    public void signIn(){
+    public void signIn(User loginUser){
         final MutableLiveData<User> data = new MutableLiveData<>();
 
-        Call<ApiResponse<User>> call =Apiservice.getInstance().apiRequest.SignIn(new User("",""));
+        Call<ApiResponse<User>> call =Apiservice.getInstance().apiRequest.SignIn(loginUser);
         call.enqueue(new Callback<ApiResponse<User>>() {
             @Override
             public void onResponse(Call<ApiResponse<User>> call, Response<ApiResponse<User>> response) {
-                if (response.body().status != "true"&&response.body().data!=null  ) {
+                if (response.body().status == "true"&&response.body().data!=null  ) {
                         data.setValue(response.body().data);
                     Log.d("tag", "articles total result:: " + response.body().getMessage());
+                }
+                else {
+                    Toast.makeText(mcontext,"auth failed", Toast.LENGTH_LONG).show();
                 }
             }
             @Override
             public void onFailure(Call<ApiResponse<User>> call, Throwable t) {
+                Log.d("tag", "articles total result:: " + t.getMessage());
 
             }
         });
