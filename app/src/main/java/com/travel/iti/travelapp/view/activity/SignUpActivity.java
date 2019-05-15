33575d1152ff9c70643b5223dc1fae0451df9ecc
EsraaRashad <resraa52@gmail.com>
2019-05-15 15:47:55 +0200

    progress bar

--- app/src/main/java/com/travel/iti/travelapp/view/activity/SignUpActivity.java
+++ app/src/main/java/com/travel/iti/travelapp/view/activity/SignUpActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -62,28 +63,44 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onChanged(@Nullable User signUpUser) {
 
-               // binding2.progressView.setVisibility(View.VISIBLE);
+
                 if (TextUtils.isEmpty(signUpUser.getUsername())) {
-                    binding2.editTextUser.setError("Enter a User Name");
+                    binding2.editTextUser.setError(getString(R.string.input_error_name));
                     binding2.editTextUser.requestFocus();
                 }
 
                 else if (TextUtils.isEmpty(signUpUser.getEmail())) {
-                    binding2.editTextEmail.setError("Enter an E-Mail Address");
+                    binding2.editTextEmail.setError(getString(R.string.input_error_email));
                     binding2.editTextEmail.requestFocus();
                 }
+                else if (!Patterns.EMAIL_ADDRESS.matcher(signUpUser.getEmail().trim()).matches()) {
+                    binding2.editTextEmail.setError(getString(R.string.input_error_email_invalid));
+                    binding2.editTextEmail.requestFocus();
+                    return;
+                }
                 else if (TextUtils.isEmpty(signUpUser.getPassword())) {
-                    binding2.editTextPassword.setError("Enter a Password");
+                    binding2.editTextPassword.setError(getString(R.string.input_error_password));
                     binding2.editTextPassword.requestFocus();
                 }
 
+                else  if (signUpUser.getPassword().length() < 6) {
+                    binding2.editTextPassword.setError(getString(R.string.input_error_password_length));
+                    binding2.editTextPassword.requestFocus();
+                    return;
+                }
+
                 else if (TextUtils.isEmpty(signUpUser.getPhone())) {
-                    binding2.editTextPhone.setError("Enter a Phone");
+                    binding2.editTextPhone.setError(getString(R.string.input_error_phone));
+                    binding2.editTextPhone.requestFocus();
+                }
+
+                else if (signUpUser.getPhone().length() != 11  ) {
+                    binding2.editTextPhone.setError(getString(R.string.input_error_phone_invalid));
                     binding2.editTextPhone.requestFocus();
                 }
 
                 else if (TextUtils.isEmpty(signUpUser.getCity())) {
-                    binding2.editTextCity.setError("Enter a City Name");
+                    binding2.editTextCity.setError(getString(R.string.input_error_city));
                     binding2.editTextCity.requestFocus();
                 }
 
@@ -104,7 +121,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     public void onChanged(@Nullable Boolean isSuccess) {
 
-        Toast.makeText(SignUpActivity.this,"isSuccess : "+ isSuccess, Toast.LENGTH_LONG).show();
+       // Toast.makeText(SignUpActivity.this,"isSuccess : "+ isSuccess, Toast.LENGTH_LONG).show();
         binding2.progressView.setVisibility(View.GONE);
         Intent intent=new Intent(SignUpActivity.this, MainActivity.class);
                     startActivity(intent);
