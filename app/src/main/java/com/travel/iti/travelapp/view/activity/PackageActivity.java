99ad5d354c87db4ac0ad8b81615f9f5e9dda0aac
EsraaRashad <resraa52@gmail.com>
2019-05-18 14:07:19 +0200

    Packages Activity

---
+++ app/src/main/java/com/travel/iti/travelapp/view/activity/PackageActivity.java
@@ -0,0 +1,16 @@
+package com.travel.iti.travelapp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.travel.iti.travelapp.R;
+
+
+public class PackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package);
+    }
+}
